0735a4ed1403edb1dd43fa8cedc3435509f1dbbf
himanshurd <91275023+himanshurd@users.noreply.github.com>
2021-10-04 00:23:53 -0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,1 +1,20 @@
+class Player
+  attr_reader :name
+  attr_reader :score
 
+  def initialize(name, score)
+    @name = name
+    @score = score
+  end
+end
+
+player = []
+
+50.times do
+  player << Player.new("Player", rand(10..300))
+end
+
+50.times do |i|
+  puts "#{player[i].name} #{i + 1}! Score: #{player[i].score}"
+  
+end
